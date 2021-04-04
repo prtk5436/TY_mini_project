96d40d2ac3dc116c449cdc2877978703832fdbde
Nishant jadhav <48583229+icodern@users.noreply.github.com>
2021-04-04 20:26:41 +0530

    UI Update BY Nishant

---
+++ app/src/main/java/com/example/tyminiproject/Onboarding3.java
@@ -0,0 +1,14 @@
+package com.example.tyminiproject;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Onboarding3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding3);
+    }
+}
