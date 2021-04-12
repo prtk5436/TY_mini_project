1e41427bf2a2663ed4741bd0e84c6dd4cfc1d317
Nishant jadhav <48583229+icodern@users.noreply.github.com>
2021-04-12 16:35:32 +0530

    2/12/2021 update Nishant

---
+++ app/src/main/java/com/example/tyminiproject/ForgotPasswordGenerateOTP.java
@@ -0,0 +1,14 @@
+package com.example.tyminiproject;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ForgotPasswordGenerateOTP extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password_generate_o_t_p);
+    }
+}
