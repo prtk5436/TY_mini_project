c7856c195c97234c5b07f5179dfc9a594599b196
Nishant jadhav <48583229+icodern@users.noreply.github.com>
2021-04-21 18:08:07 +0530

    Nishant new Updatet

--- app/src/main/java/com/example/tyminiproject/Onboarding2.java
+++ app/src/main/java/com/example/tyminiproject/Onboarding2.java
@@ -1,14 +1,36 @@
 package com.example.tyminiproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Onboarding2 extends AppCompatActivity {
 
+    private Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboarding2);
+
+        button =(Button) findViewById(R.id.onboardingNext);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
+
     }
+
+    public void openActivity2(){
+        Intent intent = new Intent(this , Onboarding3.class);
+        startActivity(intent);
+        finish();
+    }
+
+
 }
